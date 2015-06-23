74e35e2ea95b0e8eb4c55f3b8f6252cac04e8d3c
srostami <gbshabz@gmail.com>
2015-06-23 12:41:23 +0100

    Logging in works, that's nice.

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,13 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   resources :reviews
-  root 'main#index'
-
-  get'signup' => 'users#new'
   resources :users
+  
+  root 'main#index'
+  
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  get 'signup' => 'users#new'
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
