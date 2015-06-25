376fc211b178e772ed3ec103a1e0aa48a13b47ab
srostami <gbshabz@gmail.com>
2015-06-25 13:21:45 +0100

    Fixed redirect on cancel button in signup.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,9 @@
 class UsersController < ApplicationController
+  before_filter :check_for_cancel, :only => [:create]
   def new 
     @user = User.new
   end
   def create
-    if :params["cancel"]
-      redirect_to :back
-    end
     @user = User.new(user_params) 
     if @user.save 
       session[:user_id] = @user.id 
@@ -16,5 +14,10 @@ class UsersController < ApplicationController
   end
   private; def user_params
     params.require(:user).permit(:first_name,:last_name, :email, :password)
+  end
+  private; def check_for_cancel
+    if params[:commit] == "Cancel"
+      redirect_to '/'
+    end
   end
 end
